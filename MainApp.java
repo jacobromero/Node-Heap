4c5699c6f12135e86db0336b34d5a134feda9380
Jacob Romero <jacob22romero@gmail.com>
2015-04-22 00:10:43 -0700

    Implementing with java linkedlist

--- MainApp.java
+++ MainApp.java
@@ -1,9 +1,33 @@
 package edu.csupomona.cs.cs241.prog_assgmnt_1;
 
+//TODO create customer class
+//TODO implement priority queue
+
 public class MainApp {
 	public static void main(String[] args){
-		NodeHeap<Integer> test = new NodeHeap<Integer>(10);
-		test.add(5);
-		System.out.println(test.next.data);
+		NodeHeap<Integer> test = new NodeHeap<Integer>();
+		test.add(10);
+		test.add(40);
+		
+		Integer[] array = {5, 60, 3, 0, 95};
+		test.fromArray(array);
+		System.out.println(test);
+		
+		test.remove();
+		System.out.println(test);
+		test.remove();
+		test.remove();
+		System.out.println(test);
+		
+		for(int i = 0; i < array.length; i++){
+			System.out.print(array[i] + " ");
+		}
+		
+		System.out.println();
+		
+		array = test.getSortedContents(array);
+		for(int i = 0; i < array.length; i++){
+			System.out.print(array[i] + " ");
+		}
 	}
 }
--- NodeHeap.java
+++ NodeHeap.java
@@ -1,89 +1,178 @@
 package edu.csupomona.cs.cs241.prog_assgmnt_1;
 
+//TODO comment/clean up code
+
+import java.util.LinkedList;
 public class NodeHeap<V extends Comparable<V>> implements Heap<V> {
-	V data;
-	NodeHeap<V> next;
-	//TODO remove static from next to vars.
-	public NodeHeap head = null;
-	private NodeHeap last = null;
-	private int listSize = 0;
-		
-	public NodeHeap(V d){
-		data = d;
-		next = null;
-		if(head == null){
-			head = this;
-			last = this;
-		}
-		size++;
-	}
+	LinkedList<V> list = new LinkedList<V>();
+	private static int x = 0;
 	
 	@Override
 	public void add(V value) {
-		NodeHeap<V> tmp = new NodeHeap<V>(value);
-		if(head == null){
-			head = tmp;
-			last = tmp;
-			size++;
+		list.addLast(value);
+		int index = list.indexOf(value);
+		int i = list.size() - 1;
+		while(i != 0){
+			i = siftUp(value, i);
+			
+			if(i == index)
+				break;
+		}	
+	}
+
+	private int siftUp(V value, int index){
+		int parentIndex = (index - 1) / 2;
+		if(parentIndex >= 0){ x++;
+			V parent = list.get(parentIndex);
+			if(parent.compareTo(value) < 0){
+				list.set(parentIndex, value);
+				list.set(index, parent);
+			}
+			else
+				return index;
 		}
-		last.next = tmp;
-		last = tmp;
-		size++;
+		return parentIndex;
 	}
 	
-	private void siftUp(NodeHeap<V> node){
-		NodeHeap<V> current = head;
-		for(int i = 0; i > /*Get current position of passed node and use func to find paren*/; i++){
-			current = current.next;
-		}
-		//TODO finish the swap operation on the passed node, and its parent.
-		if(current.data < node.data){
-			NodeHeap<V> tmp = current;
-			current = node;
-			current.next = 
-		}
+	@Override
+	public V[] toArray() {
+		// TODO Finish this method
+		return null;
 	}
 
 	@Override
-	public V[] toArray(V[] array) {
-		int counter = 0;
-		
-		NodeHeap<V> tmp = head;
-		V[] array = new array[listSize];
-		
-		while(tmp != null){
-			array[i] = tmp.data;
-			i++;
+	public void remove() {
+		list.removeFirst();
+		V value = list.removeLast();
+		list.addFirst(value);
+		int index = 0;
+		int i = 0;
+		while(i < list.size()){
+			i = siftDown(value, i);
+			if(i == index)
+				break;
 		}
-		return array;
-	}
-
-	@Override
-	public void remove(V value) {
-		// TODO Auto-generated method stub
 		
 	}
 	
-	private void siftDown(NodeHeap<V> node){
+	public int siftDown(V value, int index){
+		int swapIndex = index;
+		int leftChildIndex = (2 * index) + 1;
+		int rightChildIndex = (2 * index) + 2;
+			
+		if(leftChildIndex < list.size()){
+			V leftChild = list.get(leftChildIndex);
+			
+			if(leftChild.compareTo(value) > 0)
+				swapIndex = leftChildIndex;
+		}
+			
+		if(rightChildIndex < list.size()){
+			V rightChild = list.get(rightChildIndex);
 		
+			if(rightChild.compareTo(list.get(swapIndex)) > 0)
+				swapIndex = rightChildIndex;
+		}
+		
+		if(swapIndex != index){
+			list.set(index, list.get(swapIndex));
+			list.set(swapIndex, value);
+			return swapIndex;
+		}
+			
+		
+		return list.size();
 	}
 
 	@Override
 	public void fromArray(V[] array) {
-		for(int i = 0; i > array.size(); i++){
-			add(array[i]);
+		for(int i = 0; i < array.length; i++){
+			this.add(array[i]);
 		}
-		
 	}
 
 	@Override
 	public V[] getSortedContents(V[] array) {
-		this.toArray()
+		int end = array.length - 1;
+		heapifiy(array);
+		
+		for(int i = end; i >= 1; i--){
+			V tmp = array[i];
+			array[i] = array[0];
+			array[0] = tmp;
+			siftDown(array, 0, i - 1);
+		}
+		return array;
 	}
 	
-	public int compareTo(V o) {
-		// TODO Auto-generated method stub
-		return 0;
+	private void siftDown(V[] array, int begIndex, int endIndex){
+		int swapIndex = begIndex;
+		int leftChildIndex = (2 * begIndex) + 1;
+		int rightChildIndex = (2 * begIndex) + 2;
+		
+		if(leftChildIndex <= endIndex){
+			V leftChild = array[leftChildIndex];
+			
+			if(leftChild.compareTo(array[begIndex]) > 0)
+				swapIndex = leftChildIndex;
+		}
+			
+		if(rightChildIndex <= endIndex){
+			V rightChild = array[rightChildIndex];
+		
+			if(rightChild.compareTo(array[swapIndex]) > 0)
+				swapIndex = rightChildIndex;
+		}
+		
+		if(swapIndex != begIndex){
+			V tmp = array[begIndex];
+			array[begIndex] = array[swapIndex];
+			array[swapIndex] = tmp;
+			
+			siftDown(array, swapIndex, endIndex);
+		}		
+	}
+	
+	private void heapifiy(V[] array) {
+		for(int i = array.length/2; i >= 0; i--){
+			siftDown(array, i);
+		}
+		
+	}
+
+	private void siftDown(V[] array, int index){
+		int swapIndex = index;
+		int leftChildIndex = (2 * index) + 1;
+		int rightChildIndex = (2 * index) + 2;
+		
+		if(leftChildIndex < array.length){
+			V leftChild = array[leftChildIndex];
+			
+			if(leftChild.compareTo(array[index]) > 0)
+				swapIndex = leftChildIndex;
+		}
+			
+		if(rightChildIndex < array.length){
+			V rightChild = array[rightChildIndex];
+		
+			if(rightChild.compareTo(array[swapIndex]) > 0)
+				swapIndex = rightChildIndex;
+		}
+		
+		if(swapIndex != index){
+			V tmp = array[index];
+			array[index] = array[swapIndex];
+			array[swapIndex] = tmp;
+			siftDown(array, swapIndex);
+		}		
+	}
+	
+	public String toString(){
+		String str = "";
+		for(int i = 0; i < list.size(); i++){
+			str = str.concat(list.get(i).toString() + " -> " );
+		}
+		return str;
 	}
 	
 }
